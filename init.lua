17a85433f75b1ea734c28f130c0fafc167173ffd
backdaniel <40582032+backdaniel@users.noreply.github.com>
2023-07-13 21:25:13 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,3 +6,27 @@ local S = minetest.get_translator("essentials_move")
 
 -- UMBRELLA
 
+minetest.register_tool("essentials_move:umbrella", {
+	description = S("Umbrella"),
+	inventory_image = "tool_umbrella.png",
+	stack_max = 1,
+	on_use = function(itemstack, user, pointed_thing)
+	end,
+})
+
+minetest.register_craft({
+	output = "essentials_move:umbrella",
+	recipe = {
+		{"default:paper", "default:paper", "default:paper"},
+		{"", "default:stick", ""},
+		{"", "default:stick", ""},
+	}
+})
+
+-- RECALL
+
+
+
+-- TELEPORT
+
+
