c9b4f63149b699f1691945aad531cc2ce270a5aa
backdaniel <40582032+backdaniel@users.noreply.github.com>
2023-07-13 23:35:44 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -29,7 +29,7 @@ minetest.register_globalstep(function(dtime)
                 player:set_wielded_item(itemstack)
 
                 local velocity = player:get_velocity()
-                player:set_physics_override({gravity=-velocity.y})
+                player:set_physics_override({gravity=velocity.y/20})
                 minetest.chat_send_all("x=" .. velocity.x .. ", y=" .. velocity.y .. ", z=" .. velocity.z)
             else
                 player:set_physics_override({gravity=1})
