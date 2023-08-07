cf6ac8e98aa27959a5365312423c4d73fb5dc4a5
Daniel <40582032+backdaniel@users.noreply.github.com>
2023-08-06 21:19:28 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -46,8 +46,8 @@ minetest.register_globalstep(function(dtime)
 			local scaled = 2 / (1 + math.exp(-difference)) - 1
 			player:add_velocity({x=0, y=scaled, z=0})
 		elseif gliding then
-				player:set_physics_override({gravity=1, speed=1})
-				gliding = false
+			player:set_physics_override({gravity=1, speed=1})
+			gliding = false
 		end
 	end
 end)
@@ -73,34 +73,34 @@ minetest.register_craft({
 local MIN_DISTANCE = 20
 
 minetest.override_item("default:mese_crystal", {
-    on_use = function(itemstack, user, pointed_thing)
-        local name = user:get_player_name()
-        local spawn
-        local current_pos = user:get_pos()
+	on_use = function(itemstack, user, pointed_thing)
+		local name = user:get_player_name()
+		local spawn
+		local current_pos = user:get_pos()
 
-        if minetest.get_modpath("beds") and beds.spawn and beds.spawn[name] then
-            spawn = beds.spawn[name]
-        else
-            spawn = minetest.setting_get_pos("static_spawnpoint")
-        end
+		if minetest.get_modpath("beds") and beds.spawn and beds.spawn[name] then
+			spawn = beds.spawn[name]
+		else
+			spawn = minetest.setting_get_pos("static_spawnpoint")
+		end
 
-        if spawn then
-            if vector.distance(current_pos, spawn) >= MIN_DISTANCE then
-                user:set_pos(spawn)
-                minetest.chat_send_all(name .. " used Mese Crystal to recall.")
-                if not is_creative(name) then
-                    itemstack:take_item()
-                end
-                return itemstack
-            else
-                minetest.chat_send_player(name, "You are too close to the spawn point!")
-                return itemstack
-            end
-        else
-            minetest.chat_send_player(name, "No available spawn point!")
-            return itemstack
-        end
-    end
+		if spawn then
+			if vector.distance(current_pos, spawn) >= MIN_DISTANCE then
+				user:set_pos(spawn)
+				minetest.chat_send_all(name .. " used Mese Crystal to recall.")
+				if not is_creative(name) then
+					itemstack:take_item()
+				end
+				return itemstack
+			else
+				minetest.chat_send_player(name, "You are too close to the spawn point!")
+				return itemstack
+			end
+		else
+			minetest.chat_send_player(name, "No available spawn point!")
+			return itemstack
+		end
+	end
 })
 
 -- TELEPORT
@@ -108,40 +108,41 @@ minetest.override_item("default:mese_crystal", {
 local texture = minetest.registered_items["default:mese_crystal_fragment"].inventory_image
 
 minetest.register_entity("movement_essentials:shard", {
-    physical = true,
-    visual = "sprite",
-    visual_size = {x=0.5, y=0.5},
-    textures = {texture},
-    on_step = function(self, dtime, moveresult)
-        if moveresult.collides then
-            local pos = self.object:get_pos()
-            for _, player in ipairs(minetest.get_connected_players()) do
-                if player:get_player_name() == self.thrower then
-                    player:set_pos(pos)
-                    self.object:remove()
-                    return
-                end
-            end
-        end
-    end,
+	physical = true,
+	collide_with_objects = false,
+	visual = "sprite",
+	visual_size = {x=0.5, y=0.5},
+	textures = {texture},
+	on_step = function(self, dtime, moveresult)
+		if moveresult.collides and moveresult.collisions[1] and moveresult.collisions[1].type == "node" then
+			local pos = self.object:get_pos()
+			for _, player in ipairs(minetest.get_connected_players()) do
+				if player:get_player_name() == self.thrower then
+					player:set_pos(pos)
+					self.object:remove()
+					return
+				end
+			end
+		end
+	end,
 })
 
 minetest.override_item("default:mese_crystal_fragment", {
-    on_use = function(itemstack, user, pointed_thing)
-        local name = user:get_player_name()
-        local playerpos = user:get_pos()
-        local obj = minetest.add_entity({
-            x = playerpos.x,
-            y = playerpos.y + 1.5,
-            z = playerpos.z,
-        }, "movement_essentials:shard")
-        local dir = user:get_look_dir()
-        obj:set_velocity({x = dir.x * 19, y = dir.y * 19, z = dir.z * 19})
-        obj:set_acceleration({x = dir.x * -3, y = -10, z = dir.z * -3})
-        obj:get_luaentity().thrower = name
-        if not is_creative(name) then
-            itemstack:take_item()
-        end
-        return itemstack
-    end
+	on_use = function(itemstack, user, pointed_thing)
+		local name = user:get_player_name()
+		local playerpos = user:get_pos()
+		local obj = minetest.add_entity({
+			x = playerpos.x,
+			y = playerpos.y + user:get_properties().eye_height,
+			z = playerpos.z,
+		}, "movement_essentials:shard")
+		local dir = user:get_look_dir()
+		obj:set_velocity({x = dir.x * 20, y = dir.y * 20, z = dir.z * 20})
+		obj:set_acceleration({x = 0, y = -8, z = 0})
+		obj:get_luaentity().thrower = name
+		if not is_creative(name) then
+			itemstack:take_item()
+		end
+		return itemstack
+	end
 })
